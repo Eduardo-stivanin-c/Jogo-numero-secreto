720a648e9a37bb39b045470bbf71de8a83d06b83
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-12-26 09:06:22 -0300

    subindo o meu site no curso da certificação da Alura

---
+++ app/jogar_de_novo.js
@@ -0,0 +1,22 @@
+//Jogar de novo
+
+//requer os outors script pra funcionar
+
+"use strick"
+
+
+
+document.body.addEventListener('click',e =>{
+    
+    
+    if (e.target.id=='jogar-novamente') {
+        //location.reloiad = windows.location.reload ous sja recarregar a a pagina
+        location.reload()
+} 
+
+
+
+}
+
+
+)
